0f156d588b336468105cb10ec5222fa1eecea3fc
HE JUNYANG <44938978+hjydmm@users.noreply.github.com>
2019-12-04 11:19:00 +0900

    Update Links.php

--- app/Model/Links.php
+++ app/Model/Links.php
@@ -4,10 +4,5 @@ namespace App\Model;
 
 class Links extends BaseModel
 {
-    //
     protected $table = 'links';
-    
-    protected $fillable = [
-        'title', 'url', 'show', 'weight','type',
-    ];
 }
