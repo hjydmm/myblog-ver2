ca6d79fa44f4aa1167954f31590dcbe826f88ed2
HE JUNYANG <44938978+hjydmm@users.noreply.github.com>
2019-12-04 11:12:50 +0900

    Update Comment.php

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -2,16 +2,12 @@
 
 namespace App\Model;
 
-use App\Model\Article;
-
 class Comment extends BaseModel
 {
-
     protected $table = 'comments';
 
     public function articles()
     {
         return $this->belongsTo(Article::class, 'aid', 'id');
     }
-
 }
