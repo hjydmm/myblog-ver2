dc221df4a0a7706d6d9747738b7c82560f3ddcee
HE JUNYANG <44938978+hjydmm@users.noreply.github.com>
2019-12-04 10:27:56 +0900

    Update ArticleRelate.php

--- app/Model/ArticleRelate.php
+++ app/Model/ArticleRelate.php
@@ -1,19 +1,11 @@
 <?php
 
 namespace App\Model;
-use App\Model\Article;
-use App\Model\Users;
 
 class ArticleRelate extends BaseModel
 {
-
     protected $table = 'article_relate';
 
-    /**
-     * author: カ シュンヨウ
-     * description: 通过文章附加关联指定文章(一对一)
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
     public function articles()
     {
         return $this->belongsTo(Article::class, 'aid', 'id');
@@ -23,5 +15,4 @@ class ArticleRelate extends BaseModel
     {
         return $this->belongsTo(Users::class, 'user_id', 'id');
     }
-
 }
