0e206bf2c84ecafc6dc830f71407d2ace528302f
HE JUNYANG <44938978+hjydmm@users.noreply.github.com>
2019-12-05 10:43:26 +0900

    Update Users.php

--- app/Model/Users.php
+++ app/Model/Users.php
@@ -3,37 +3,21 @@
 namespace App\Model;
 
 use Illuminate\Auth\Authenticatable;
-use App\Model\Article;
-use App\Model\ArticleRelate;
-use App\Model\Like;
-use App\Model\Store;
-use App\Model\Comment;
-use App\Model\Attend;
-
 
 class Users extends BaseModel implements \Illuminate\Contracts\Auth\Authenticatable
 {
     protected $table = 'users';
 
     use Authenticatable;
-    /**
-     * author: カ シュンヨウ
-     * description: 关联文章article(一对多)
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
+
     public function articles()
     {
         return $this->hasMany(Article::class, 'user_id', 'id');
     }
 
-    /**
-     * author: カ シュンヨウ
-     * description: 关联文章附加信息article_relate(远程一对多)
-     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
-     */
     public function article_relate()
     {
-        return $this->hasManyThrough(ArticleRelate::class, Article::class, 'user_id', 'aid', 'id', 'id');
+        return $this->hasManyThrough(ArticleRelate::class, Article::class, 'user_id', 'aid', 'id');
     }
 
     public function like()
@@ -53,6 +37,6 @@ class Users extends BaseModel implements \Illuminate\Contracts\Auth\Authenticata
 
     public function attend()
     {
-        return $this->hasMany(Comment::class, 'user_id', 'id');
+        return $this->hasMany(Attend::class, 'user_id', 'id');
     }
 }
