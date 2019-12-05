80e9ed4e0d4085054e948c9a8f75372e5f87cf7f
HE JUNYANG <44938978+hjydmm@users.noreply.github.com>
2019-12-05 11:54:59 +0900

    Update Response.php

--- app/Traits/Response.php
+++ app/Traits/Response.php
@@ -4,13 +4,6 @@ namespace App\Traits;
 
 Trait Response
 {
-    /**
-     *
-     * @author:wuyanwen
-     * @description:ajax错误相应
-     * @date:2017年9月3日
-     * @param unknown $data
-     */
     protected function ajaxError($msg = '', array $data = [])
     {
         return response()->json([
@@ -20,13 +13,6 @@ Trait Response
         ]);
     }
     
-    /**
-     *
-     * @author:wuyanwen
-     * @description:ajax成功相应
-     * @date:2017年9月3日
-     * @param unknown $data
-     */
     protected function ajaxSuccess($msg = '', array $data = [])
     {
         return response()->json([
@@ -36,13 +22,10 @@ Trait Response
         ]);
     }
     
-    /**
-     * @description:http 响应错误
-     * @author wuyanwen(2017年9月28日)
-     * @param unknown $code
-     * @param unknown $message
-     */
-    protected function error($code, $message = 'Page Not Found')
+    //abort(404, "ページが見つかりません");
+    //resources/views/errors/404.blade.php
+    //<h2>{{ $exception->getMessage() }}</h2>
+    protected function error($code, $message = '')
     {
        return abort($code, $message);
     }
