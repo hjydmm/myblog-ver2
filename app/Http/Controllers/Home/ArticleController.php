8725599c7c48d9751200be6b313f0f791427a405
HE JUNYANG <44938978+hjydmm@users.noreply.github.com>
2019-12-05 15:20:35 +0900

    Update ArticleController.php

--- app/Http/Controllers/Home/ArticleController.php
+++ app/Http/Controllers/Home/ArticleController.php
@@ -3,20 +3,13 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Requests\Request;
-use App\Http\Controllers\Controller;
 use App\Services\UsersServiceInterface;
 use App\Services\ArticleServiceInterface;
 use App\Services\CategoryServiceInterface;
 use App\Services\CategoriesServiceInterface;
 use App\Services\TagServiceInterface;
 use App\Services\TagsServiceInterface;
-use App\Http\Controllers\Home\UserController;
 use App\Traits\Response;
-
-//之后要删除
-use App\Repositories\ArticleRepository;
-use App\Repositories\CategoryRepository;
-
 
 class ArticleController extends BaseController
 {
@@ -61,7 +54,6 @@ class ArticleController extends BaseController
      */
     public function articleDetail($id)
     {
-
         if(isset($_SERVER['HTTP_REFERER']) && strstr($_SERVER['HTTP_REFERER'], "/series/") !== false) {
             $before_page = "series";
         }elseif(isset($_SERVER['HTTP_REFERER']) && strstr($_SERVER['HTTP_REFERER'], "/rankings/") !== false) {
@@ -69,7 +61,6 @@ class ArticleController extends BaseController
         }else {
             $before_page = "normal";
         }
-
         $article_info = $this->articleService->getArticleInfoById($id);
         $tagsArray = $this->tagsService->stringToArray(',', $article_info->tags->str_tags);
         $categoriesArray = $this->categoriesService->stringToArray(',', $article_info->categories->str_categories);
@@ -94,9 +85,7 @@ class ArticleController extends BaseController
         $user = $this->userService->getUserById($id);
         $pageElement = $this->userController->navPage();
         $tagData = $this->tagService->getAlltag();
-        //自定义返回的分类，标签的背景颜色
-        $button_rand = array("btn-primary", "btn-info", "btn-warning");
-        return view('home.article.draft', compact('pageElement', 'tagData', 'button_rand', 'article_info', 'id', 'user', 'tagsIdsArray', 'categoriesIdsArray'));
+        return view('home.article.draft', compact('pageElement', 'tagData', 'article_info', 'id', 'user', 'tagsIdsArray', 'categoriesIdsArray'));
     }
 
     public function getSeries() {
@@ -105,11 +94,11 @@ class ArticleController extends BaseController
         $articles = $this->articleService->getContainSeriesList($series_name);
         return view('home.user.seriesArticles',compact('pageElement', 'articles', 'series_name'));
     }
+    
     public function getRankings() {
         $rankings_name = $this->request->route('rankings');
         $pageElement = $this->userController->navPage();
         $articles = $this->articleService->getContainRankingsList($rankings_name);
         return view('home.user.rankingsArticles',compact('pageElement', 'articles', 'rankings_name'));
     }
-
 }
